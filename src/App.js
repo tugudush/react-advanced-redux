b7dc6e4c663d2d21e7c16723d42ae29d1e589b2f
tugudush <jerome2kph@gmail.com>
2024-06-30 13:05:17 +0800

    fixed products map key

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);  
+  console.log(cart);
+  useEffect(() => {
+    
+  }, [cart])
+
   console.log("showCart: ", showCart);
   return (
     <Layout>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -20,12 +20,7 @@ const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+      <ul>        
         {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
             key={product.id}
